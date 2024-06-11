0db04e3296e25b46f03e85add2eb85b77fc04d41
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-06-11 14:17:56 +0530

    commit

--- start_api_load.sh
+++ start_api_load.sh
@@ -9,6 +9,16 @@ curr_time="$(date +'%Y-%m-%d_%H-%M-%S')"
 echo "Enter domain name : "
 read domain
 
+properties_path="scripts/domain/${domain}.properties"
+
+# Check if the file exists
+if [ -f "$properties_path" ]; then
+    echo "File '$properties_path' exists."
+else
+    echo "File '$properties_path' does not exist."
+    exit 1
+fi
+
 echo "enter duration of the load (in sec) : "
 read DURATION
 
